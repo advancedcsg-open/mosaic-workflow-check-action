b15e7c2f83f9673f4f8d2af41f4e513c34865e85
pkrawiec <pkrawiec@pgs-soft.com>
2021-04-28 16:04:47 +0200

    initial commit

--- README.md
+++ README.md
@@ -1,2 +1,38 @@
-# mosaic-workflow-check-action
-Action that checks the last execution of a workflow for statuses and conclusions
+# Workflow Check action
+
+Checks the last execution of a workflow for statuses and conclusions.
+
+## Inputs
+
+### `workflow`
+
+**Required** The ID of the workflow. You can also pass the workflow file name as a string.
+
+### `passing-statuses`
+
+**Optional** A list of workflow statuses that will pass this action, if not provided, the action will succeed. If provided, both passing conditions need to be fulfilled. Can be one of: “queued”, “in_progress”, or “completed”.
+
+### `passing-conclusions`
+
+**Optional** A list of workflow conclusions that will pass this action, if not provided, the action will succeed. If provided, both passing conditions need to be fulfilled. Can be one of the “success”, “failure”, “neutral”, “cancelled”, “skipped”, “timed_out”, or “action_required”.
+
+## Outputs
+
+### `conclusion`
+
+Can be one of the “success”, “failure”, “neutral”, “cancelled”, “skipped”, “timed_out”, or “action_required”.
+
+### `status`
+
+Can be one of: “queued”, “in_progress”, or “completed”.
+
+## Example usage
+```
+uses: advancedcsg-open/mosaic-workflow-check-action@v1.0
+with:
+    workflow: 'sample-workflow.yml'
+    passing-statuses: 'completed'
+    passing-conclusions: 'success,error'
+env:
+    GITHUB_TOKEN: ${{ secrets.GITHUB_TOKEN }}
+```
---
+++ index.js
@@ -0,0 +1,48 @@
+const core = require('@actions/core');
+const github = require('@actions/github');
+const octokit = github.getOctokit(process.env.GITHUB_TOKEN)
+
+try {
+    const workflow = core.getInput('workflow');
+    let statusesString = core.getInput('passing-statuses');
+    let conclusionsString = core.getInput('passing-conclusions');
+
+    let statuses = statusesString ? statusesString.split(',') : null
+    let conclusions = conclusionsString ? conclusionsString.split(',') : null
+
+    let workflowRuns = octokit.actions.listWorkflowRuns({
+        owner: github.context.repo.owner,
+        repo: github.context.repo.repo,
+        workflow_id: workflow,
+        per_page: 1
+    });
+    workflowRuns.then((response => {
+        console.log(response)
+        let data = response['data']
+        console.log(`total count: ${data["total_count"]}`)
+        if (data['total_count'] > 0) {
+            let workflowRun = data['workflow_runs'][0]
+            let workflowConclusion = workflowRun['conclusion']
+            let workflowStatus = workflowRun['status']
+            let message = `Workflow ${workflow} is in status ${workflowStatus} with a conclusion of ${workflowConclusion}`
+            console.info(message)
+
+            if ((conclusions == null || conclusions.includes(workflowConclusion)) && (statuses == null || statuses.includes(workflowStatus))) {
+                core.setOutput("conclusion", workflowConclusion);
+                core.setOutput("status", workflowStatus);
+            } else {
+                core.setFailed(message);
+            }
+        } else {
+            let message = `Workflow ${workflow} has no previous executions`
+            console.error(error)
+            core.setFailed(message);
+        }
+    })).catch((error) => {
+        console.error(error)
+        core.setFailed(error);
+    });
+
+} catch (error) {
+    core.setFailed(error.message);
+}
